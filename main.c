b68939582e2d1a0b5fb1da1018bff4cbdf4e56dc
myumally <58273089+myumally@users.noreply.github.com>
2024-03-24 19:12:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,32 +10,24 @@
 
 #include "const.h"
 
-/* VARIABLES GLOBALES */
-
-SDL_Window   * window   = NULL; // fenetre utilisee par le programme
-SDL_Renderer * renderer = NULL; // moteur de rendu SDL
-SDL_Texture  * logo     = NULL; // image chargee
-TTF_Font     * font1    = NULL; // polices de caracteres
-TTF_Font     * font2    = NULL; 
-
-struct boite** gra=NULL; //graphe
+struct boite** graphe=NULL; //graphe
 
 int main (){
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
-    SDL_Window   * w=creafenetre();
-    TTF_Font * font1=TTF_OpenFont(FONT, 72 );
+    SDL_Window   * window=creafenetre();
+    TTF_Font * font=TTF_OpenFont(FONT, 72 );
     FILE* fichier=NULL;
     fichier=fopen(NOM_DU_FICHIER, "r");
     if (fichier){
-        rungraphe_sdl(fichier, &gra, w, font1);
+        rungraphe_sdl(fichier, &graphe, window, font);
     }
     else{
         // On affiche un message d'erreur si on veut
         printf("Impossible d'ouvrir le fichier");
     }
     int fclose(FILE* fic);
-    TTF_CloseFont(font1);
+    TTF_CloseFont(font);
     SDL_DestroyWindow(window);
     SDL_Quit();
 

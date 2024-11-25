e916918344b707b76c4f68ca8e42882ea7329fb3
Domi <domitillevale@gmail.com>
2024-11-25 17:00:24 +0100

    Toujours une horreur mais possibilité de choisir entre run dans le terminal ou avec l'interface sdl un peu douteuse

--- main.c
+++ main.c
@@ -12,24 +12,74 @@
 
 struct boite** graphe=NULL; //graphe
 
-int main (){
-    SDL_Init(SDL_INIT_EVERYTHING);
-    TTF_Init();
-    SDL_Window   * window=creafenetre();
-    TTF_Font * font=TTF_OpenFont(FONT, 72 );
-    FILE* fichier=NULL;
-    fichier=fopen(NOM_DU_FICHIER, "r");
-    if (fichier){
-        rungraphe_sdl(fichier, &graphe, window, font);
-    }
-    else{
-        // On affiche un message d'erreur si on veut
-        printf("Impossible d'ouvrir le fichier");
-    }
-    int fclose(FILE* fic);
-    TTF_CloseFont(font);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+int main (int argc, char **argv){
 
-    return 2;
+	if(argc==2){
+        if(strcmp(argv[1], "sdl") == 0){
+
+            SDL_Init(SDL_INIT_EVERYTHING);
+            TTF_Init();
+            SDL_Window   * window=creafenetre();
+            TTF_Font * font=TTF_OpenFont(FONT, 72 );
+
+            FILE* fichier=NULL;
+            fichier=fopen(NOM_DU_FICHIER, "r");
+
+            if (fichier){
+                rungraphe_sdl(fichier, &graphe, window, font);
+            }
+
+            else{
+                // On affiche un message d'erreur si on veut
+                printf("Impossible d'ouvrir le fichier");
+            }
+
+            int fclose(FILE* fic);
+
+            TTF_CloseFont(font);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+        }
+    }
+    
+
+    else if (argc==1){
+
+        FILE* fichier=NULL;
+        fichier=fopen(NOM_DU_FICHIER, "r");
+
+        if (fichier){
+
+            int c=stockage (fichier, &graphe);
+            struct boite** cur=graphe;
+
+            struct boite* b=*(cur);
+            struct reponse* r=NULL;
+
+            int rep=1;
+
+            while(rep){
+                printf("%s",b->diag);
+                for (int i=0;i<b->nbrep;i++){
+                    r=*((b->rep)+(i));
+                    printf("%d : %s", i+1, r->text);
+                }
+                scanf("%d", &rep);
+                if(rep){
+                    cur=(graphe)+(*((((*cur)->rep)+(rep-1))))->next;
+                    b=*(cur);
+                }
+            }
+
+            destockage(c,graphe);
+        }
+
+        else{
+            // On affiche un message d'erreur si on veut
+            printf("Impossible d'ouvrir le fichier");
+        }
+
+        int fclose(FILE* fic);
+    }
+    return 0;
 }

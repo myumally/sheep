8bd19e447925e00aed14f5253fa491aaec91c893
Domi <domitillevale@gmail.com>
2025-04-25 21:47:42 +0200

    augmented number of character accepted, cleaned useless parameter previously used for sdl2

--- Web_Assembly/const.h
+++ Web_Assembly/const.h
@@ -1,57 +1,10 @@
 #ifndef CONST_H
 #define CONST_H
 
-
-//parametre globaux
-
 #define NOM_DU_FICHIER "test.txt"
 #define NOMBRE_MAX_DE_BOITE_DE_DIALOGUE 999
-#define NOMBRE_MAX_DE_CARACTERE_PAR_BOITE_DE_DIALOGUE 99
+#define NOMBRE_MAX_DE_CARACTERE_PAR_BOITE_DE_DIALOGUE 999
 #define NOMBRE_MAX_DE_REPONSE_POSSIBLE 9
-#define NOMBRE_MAX_DE_CARACTERE_PAR_REPONSE 99
-
-
-//police d'écriture
-
-#define FONT "Unique.ttf"
-
-
-//parametre fenetre
-
-#define WIDTH 1080
-#define HEIGHT 600
-
-
-//parametre boite de dialogue
-
-#define WIDTH_DIAG 1080
-#define HEIGHT_DIAG 300
-
-#define COULEUR_DIAG_R 0
-#define COULEUR_DIAG_G 255
-#define COULEUR_DIAG_B 255
-#define COULEUR_DIAG_A 255
-
-#define COULEUR_DIAG_TEXT_R 0
-#define COULEUR_DIAG_TEXT_G 0
-#define COULEUR_DIAG_TEXT_B 0
-#define COULEUR_DIAG_TEXT_A 255
-
-
-//parametre boutons reponses
-
-#define WIDTH_REP 200
-#define HEIGHT_REP 100
-
-#define COULEUR_REP_R 200
-#define COULEUR_REP_G 150
-#define COULEUR_REP_B 20
-#define COULEUR_REP_A 255
-
-#define COULEUR_REP_TEXT_R 0
-#define COULEUR_REP_TEXT_G 0
-#define COULEUR_REP_TEXT_B 0
-#define COULEUR_REP_TEXT_A 255
-
+#define NOMBRE_MAX_DE_CARACTERE_PAR_REPONSE 999
 
 #endif
